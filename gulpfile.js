f68c048f7f95d7e9d85e6ac5c65a535fabb99a96
Daisuke Sasaki <gerund25495641@gmail.com>
2022-02-06 10:45:55 +0900

    minjsでJSのコンパイルを実装

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 let gulp = require('gulp');
 let cleanCSS = require('gulp-clean-css');
-var rename   = require("gulp-rename");
+let rename   = require("gulp-rename");
+let uglify = require('gulp-uglify');
 
 gulp.task('default', function() {
 	console.log('test_implementation');
@@ -11,4 +12,13 @@ gulp.task('mincss', function() {
 		.pipe(cleanCSS())
 		.pipe(rename({ suffix: '.min' }))
 		.pipe(gulp.dest('css/'));
+});
+
+
+
+gulp.task('minjs', function() {
+	return gulp.src("js/*.js")
+		.pipe(uglify())
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(gulp.dest('js/'));
 });
