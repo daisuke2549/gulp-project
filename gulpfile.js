9c88d5d685f9562d6621f1c23a77f164230dab04
Daisuke Sasaki <gerund25495641@gmail.com>
2022-02-07 11:25:50 +0900

    JavaScript圧縮のプラグインを実装

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,35 @@ let cleanCSS = require('gulp-clean-css');
 let rename   = require("gulp-rename");
 let uglify = require('gulp-uglify');
 
+const imagemin = require("gulp-imagemin");
+const pngquant = require("imagemin-pngquant");
+
+function minifyImage() {
+  return (
+    gulp
+      // 圧縮前の画像を入れるフォルダを指定
+      .src("./base/img/*.{png,jpg,jpeg,gif,svg}")
+      // 圧縮率などを設定
+      .pipe(
+        imagemin([
+          imagemin.mozjpeg({ quality: 80 }),
+          imagemin.svgo({
+            plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+          }),
+          imagemin.gifsicle({
+            interlaced: true,
+          }),
+          pngquant({
+            quality: [0.65, 0.8],
+            speed: 1,
+          }),
+        ])
+      )
+      // 圧縮後の画像を出力するフルダを指定
+      .pipe(gulp.dest("./base/"))
+  );
+}
+
 gulp.task('default', function() {
 	console.log('test_implementation');
 });
